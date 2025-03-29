287274264f8ba118802388d5f71daa54f31a0f27
DEVD04 <debdyutidas0@gmail.com>
2025-03-29 23:33:20 +0530

    fixed select *, insert('string'), added is/not null support

--- SQL_Query_Parser_LL1/src/Lexer.java
+++ SQL_Query_Parser_LL1/src/Lexer.java
@@ -16,13 +16,13 @@ class Lexer {
         int i = 0;
         while (i < input.length()) {
             char c = input.charAt(i);
-
+    
             // Skip whitespace
             if (Character.isWhitespace(c)) {
                 i++;
                 continue;
             }
-
+    
             // Handle symbols
             switch (c) {
                 case '*': tokens.add(new Token(TokenType.STAR, "*")); i++; continue;
@@ -34,50 +34,58 @@ class Lexer {
                 case '>': tokens.add(new Token(TokenType.GREATER, ">")); i++; continue;
                 case '<': tokens.add(new Token(TokenType.LESS, "<")); i++; continue;
             }
-
-            // Handle identifiers and keywords
-            if (Character.isLetter(c)) {
-                int start = i;
-                while (i < input.length() && (Character.isLetterOrDigit(input.charAt(i)) || input.charAt(i) == '_')) {
+    
+            // Handle string literals (single-quoted)
+            if (c == '\'') {
+                int start = i + 1;  // Skip opening quote
+                i++;
+                while (i < input.length() && input.charAt(i) != '\'') {
                     i++;
                 }
-                String word = input.substring(start, i).toUpperCase();
-
-                // Keywords and special tokens
-                switch (word) {
-                    case "SELECT": tokens.add(new Token(TokenType.SELECT, word)); continue;
-                    case "FROM": tokens.add(new Token(TokenType.FROM, word)); continue;
-                    case "WHERE": tokens.add(new Token(TokenType.WHERE, word)); continue;
-                    case "ORDER":
-                        if (i + 2 < input.length() && input.substring(i, i + 3).equalsIgnoreCase(" BY")) {
-                            tokens.add(new Token(TokenType.ORDER, "ORDER"));
-                            tokens.add(new Token(TokenType.BY, "BY"));
-                            i += 3;
-                            continue;
-                        }
-                        break;
-                    case "BY": tokens.add(new Token(TokenType.BY, word)); continue;
-                    case "GROUP":
-                        if (i + 2 < input.length() && input.substring(i, i + 3).equalsIgnoreCase(" BY")) {
-                            tokens.add(new Token(TokenType.GROUP, "GROUP"));
-                            tokens.add(new Token(TokenType.BY, "BY"));
-                            i += 3;
-                            continue;
-                        }
-                        break;
-                    case "ASC": tokens.add(new Token(TokenType.ASC, word)); continue;
-                    case "DESC": tokens.add(new Token(TokenType.DESC, word)); continue;
-                    case "INSERT": tokens.add(new Token(TokenType.INSERT, word)); continue;
-                    case "INTO": tokens.add(new Token(TokenType.INTO, word)); continue;
-                    case "VALUES": tokens.add(new Token(TokenType.VALUES, word)); continue;
-                    case "DELETE": tokens.add(new Token(TokenType.DELETE, word)); continue;
-                    case "AND": tokens.add(new Token(TokenType.AND, word)); continue;
-                    case "OR": tokens.add(new Token(TokenType.OR, word)); continue;
-                    case "NOT": tokens.add(new Token(TokenType.NOT, word)); continue;
-                    default: tokens.add(new Token(TokenType.IDENTIFIER, word)); continue;
+    
+                if (i >= input.length()) {
+                    throw new RuntimeException("Syntax Error: Unterminated string literal");
                 }
+    
+                String stringValue = input.substring(start, i); // Extract string content
+                tokens.add(new Token(TokenType.STRING, stringValue));
+                i++; // Skip closing quote
+                continue;
             }
+    
+            // Handle identifiers and keywords
+            // Handle identifiers and keywords
+        if (Character.isLetter(c)) {
+            int start = i;
+            while (i < input.length() && (Character.isLetterOrDigit(input.charAt(i)) || input.charAt(i) == '_')) {
+                i++;
+            }
+            String word = input.substring(start, i).toUpperCase();
 
+            // Keywords
+            switch (word) {
+                case "SELECT": tokens.add(new Token(TokenType.SELECT, word)); continue;
+                case "FROM": tokens.add(new Token(TokenType.FROM, word)); continue;
+                case "WHERE": tokens.add(new Token(TokenType.WHERE, word)); continue;
+                case "ORDER": tokens.add(new Token(TokenType.ORDER, word)); continue;
+                case "BY": tokens.add(new Token(TokenType.BY, word)); continue;
+                case "GROUP": tokens.add(new Token(TokenType.GROUP, word)); continue;
+                case "ASC": tokens.add(new Token(TokenType.ASC, word)); continue;
+                case "DESC": tokens.add(new Token(TokenType.DESC, word)); continue;
+                case "INSERT": tokens.add(new Token(TokenType.INSERT, word)); continue;
+                case "INTO": tokens.add(new Token(TokenType.INTO, word)); continue;
+                case "VALUES": tokens.add(new Token(TokenType.VALUES, word)); continue;
+                case "DELETE": tokens.add(new Token(TokenType.DELETE, word)); continue;
+                case "AND": tokens.add(new Token(TokenType.AND, word)); continue;
+                case "OR": tokens.add(new Token(TokenType.OR, word)); continue;
+                case "NOT": tokens.add(new Token(TokenType.NOT, word)); continue;
+                case "IS": tokens.add(new Token(TokenType.IS, word)); continue;
+                case "NULL": tokens.add(new Token(TokenType.NULL, word)); continue;
+                default: tokens.add(new Token(TokenType.IDENTIFIER, word)); continue;
+            }
+        }
+
+            
             // Handle numbers
             if (Character.isDigit(c)) {
                 int start = i;
@@ -87,13 +95,15 @@ class Lexer {
                 tokens.add(new Token(TokenType.NUMBER, input.substring(start, i)));
                 continue;
             }
-
+    
             // If we reach here, it's an unknown token
             tokens.add(new Token(TokenType.UNKNOWN, String.valueOf(c)));
             i++;
         }
+        tokens.add(new Token(TokenType.END, "EOF")); // End marker
         System.out.println(tokens);
     }
+    
 
     public Token getNextToken() {
         return index < tokens.size() ? tokens.get(index++) : new Token(TokenType.END, "EOF");
